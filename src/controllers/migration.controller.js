e595e48cf7123cbb573f1b49b8a515792c349e14
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-27 23:37:01 +0530

    Integrate migration service in createMigration method

--- src/controllers/migration.controller.js
+++ src/controllers/migration.controller.js
@@ -1,4 +1,6 @@
+const migrationService = require('../services/migration.service');
 
 exports.createMigration = async (req, res) => {
-  res.json({ status: 'started' });
+  const result = await migrationService.startMigration(req.body);
+  res.json(result);
 };
