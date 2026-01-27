a53d9522532bc28ade7943d42d99aa6c8da7a83f
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-28 02:12:01 +0530

    Update migration.routes.js

--- src/routes/migration.routes.js
+++ src/routes/migration.routes.js
@@ -1,15 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const migrationController = require('../controllers/migration.controller');
+
 router.get('/test', (req, res) => {
   res.json({
     status: 'ok',
     message: 'Migration API is working'
   });
 });
-
-
-const express = require('express');
-const router = express.Router();
-
-const migrationController = require('../controllers/migration.controller');
 
 router.post('/migrate', migrationController.createMigration);
 
