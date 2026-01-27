4341c94036a1235aa9451be2f88a1cdacfd74df8
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-27 23:43:17 +0530

    Update fakeQueue.js

--- src/utils/fakeQueue.js
+++ src/utils/fakeQueue.js
@@ -1,1 +1,10 @@
 
+const queue = [];
+
+exports.addJob = (job) => {
+  queue.push(job);
+  process.nextTick(() => {
+    const worker = require('../workers/migration.worker');
+    worker.process(job);
+  });
+};
