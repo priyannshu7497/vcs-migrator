eb6a8434cee612c364d2549909c3ddc4a4f52b1b
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-28 01:18:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,3 +2,6 @@ console.log("Worker started job:", jobId);
 console.log("Sync repositories");
 console.log("Sync branches");
 console.log("Sync completed for job:", jobId);
+
+setInterval(() => {}, 1000);
+
