87f6dd9ecf42aebcffccbcd4b6fa0555a9b0f913
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-28 01:43:20 +0530

    Refactor job queuing and processing logic
    Refactor job queue implementation to use a processor function.

--- src/utils/fakeQueue.js
+++ src/utils/fakeQueue.js
@@ -1,10 +1,18 @@
-
-const queue = [];
+let jobs = [];
+let processor = null;
 
 exports.addJob = (job) => {
-  queue.push(job);
-  process.nextTick(() => {
-    const worker = require('../workers/migration.worker');
-    worker.process(job);
-  });
+  console.log(`📥 Job queued: ${job.id}`);
+  jobs.push(job);
+
+  if (processor) {
+    processor(job);
+  } else {
+    console.log('⚠️ No worker registered yet');
+  }
+};
+
+exports.processJobs = (fn) => {
+  console.log('🧵 Worker registered');
+  processor = fn;
 };
