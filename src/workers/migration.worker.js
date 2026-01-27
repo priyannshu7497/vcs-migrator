53a06a0800e3c3aee7ae8e3a69551a4387cb67a8
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-27 23:44:19 +0530

    Update migration.worker.js

--- src/workers/migration.worker.js
+++ src/workers/migration.worker.js
@@ -1,1 +1,14 @@
 
+exports.process = async (job) => {
+  console.log('Worker started job:', job.id);
+
+  // Simulate steps
+  await new Promise(r => setTimeout(r, 500));
+  console.log('Sync repositories');
+
+  await new Promise(r => setTimeout(r, 500));
+  console.log('Sync branches');
+
+  await new Promise(r => setTimeout(r, 500));
+  console.log('Sync completed for job:', job.id);
+};
