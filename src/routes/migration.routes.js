110e1cf7f88eef422637e02cb79d3c4689d72ac2
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-27 23:34:32 +0530

    Update migration.routes.js

--- src/routes/migration.routes.js
+++ src/routes/migration.routes.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+
+const migrationController = require('../controllers/migration.controller');
+
+router.post('/migrate', migrationController.createMigration);
+
+module.exports = router;
