0a599c45fe7ee65de366455c5e90ff7747ef3ba9
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-28 01:42:05 +0530

    Refactor dotenv usage in app.js
    Removed redundant dotenv import and configuration.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
+require('dotenv').config();
 require('./workers/migration.worker');
 
 const express = require('express');
-const dotenv = require('dotenv');
 const migrationRoutes = require('./routes/migration.routes');
-
-dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -12,7 +10,6 @@ app.use(express.json());
 app.use('/migrations', migrationRoutes);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
