61c858ab0f4a12c21052d907453ba1f0546ba569
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-28 11:03:16 +0530

    Update migration.py

--- app/routes/migration.py
+++ app/routes/migration.py
@@ -1,8 +1,7 @@
-
 from fastapi import APIRouter
 from app.services.migration_service import start_migration
 
-router = APIRouter(prefix="/migrations")
+router = APIRouter(prefix="/migrations", tags=["Migration"])
 
 @router.post("/migrate")
 def migrate(payload: dict):
