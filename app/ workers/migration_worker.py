efa7fda1df1cb2931b00c4ebe4031767197daa95
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-28 10:58:50 +0530

    Update migration_worker.py

--- app/ workers/migration_worker.py
+++ app/ workers/migration_worker.py
@@ -1,1 +1,12 @@
 
+from celery import Celery
+
+celery_app = Celery(
+    "migrator",
+    broker="redis://localhost:6379/0"
+)
+
+@celery_app.task
+def run_migration(payload):
+    print("Starting migration:", payload)
+    # Here Git APIs will be called
