e6af07ac8ce210e32aaaa144198eae23a4cb4c10
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-28 10:50:47 +0530

    Delete src/controllers/migration.controller.js

--- src/controllers/migration.controller.js
+++
@@ -1,6 +0,0 @@
-const migrationService = require('../services/migration.service');
-
-exports.createMigration = async (req, res) => {
-  const result = await migrationService.startMigration(req.body);
-  res.json(result);
-};
