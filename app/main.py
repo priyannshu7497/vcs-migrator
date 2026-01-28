296fabc880e6aa55d13535647543bd98fdf7f1eb
Priyanshu Gupt <gpriyannshu@gmail.com>
2026-01-28 10:57:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
 
+from fastapi import FastAPI
+from app.routes.migration import router
+
+app = FastAPI(title="VCS Migrator")
+
+app.include_router(router)
+
+@app.get("/")
+def health():
+    return {"status": "running"}
